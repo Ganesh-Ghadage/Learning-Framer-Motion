f1f4082661a9f4b935c841f2dbac7749c33231ba
Ganesh Ghadage <ganeshvg22@gmail.com>
2024-09-23 20:15:58 +0530

    svg animation

--- pizza-joint/src/components/Header.jsx
+++ pizza-joint/src/components/Header.jsx
@@ -2,27 +2,57 @@ import React from 'react';
 import { motion } from 'framer-motion'
 import { Link } from 'react-router-dom';
 
+const svgVarient = {
+  hidden: {
+    rotate: -180,
+  },
+  visible: {
+    rotate: 0,
+    transition: {
+      duration: 1,
+    },
+  },
+}
+
+const pathVarient = {
+  hidden: {
+    pathLength: 0,
+    opacity: 0,
+  },
+  visible: {
+    pathLength: 1,
+    opacity: 1,
+    transition: {
+      duration: 2,
+      ease: 'easeInOut',
+    },
+  },
+}
+
 const Header = () => {
   return (
     <header>
       <motion.div className="logo"
-        initial={{zoom: 0}}
-        animate={{zoom: 1, }}
-        transition={{duration: 0.6, originx: 0}}
         whileHover={{
           scale: 1.1,
         }}
       >
-        <svg className="pizza-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-          <path
+        <motion.svg className="pizza-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"
+          variants={svgVarient}
+          initial="hidden"
+          animate="visible"
+        >
+          <motion.path
             fill="none"
             d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
+            variants={pathVarient}
           />
-          <path
+          <motion.path
             fill="none"
             d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
+            variants={pathVarient}
           />
-        </svg>
+        </motion.svg>
       </motion.div>
       <motion.div className="title"
         initial={{y: -250}}
